1c8c3858d440f228b9f868df302d7aa1d24d9171
iabdelnabi <metigator@gmail.com>
2022-06-24 07:05:30 -0400

    initial commit

---
+++ OddEvenNumbersApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OddEvenNumbersApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            PrintNumbers("Numbers", numbers);
+            PrintNumbers("Even No", numbers.Where(x=> IsEven(x)));
+            PrintNumbers("Odd No", numbers.Where(x => IsOdd(x)));
+
+            Console.ReadKey();
+        }
+
+        static void PrintNumbers (string title, IEnumerable<int> numbers)
+        {
+            Console.WriteLine(); 
+            Console.Write($"{title}: [");
+            foreach (var n in numbers)
+            {
+                Console.Write($" {n}");
+            }
+            Console.Write($" ]");
+            Console.WriteLine();
+        }
+
+        static bool IsEven(int number)
+        {
+            if( number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool IsOdd(int number)
+        {
+            if (number % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
